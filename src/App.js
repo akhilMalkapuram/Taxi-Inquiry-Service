ee1c6aefff166ac064a8241340c09377129e98aa
akhilMalkapuram <malkapuramakhil2196@gmail.com>
2023-04-22 08:33:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,79 @@
-import Keypad from "./components/Keypad"
-
-import './App.css'
-import { useState } from "react"
+import React, { useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [pickupLocation, setPickupLocation] = useState('');
+  const [dropoffLocation, setDropoffLocation] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [additionalRequirements, setAdditionalRequirements] = useState('');
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (pickupLocation.trim() === '' || dropoffLocation.trim() === '' || date.trim() === '' || time.trim() === '') {
+      alert('Please fill in all required fields.');
+    } else {
+      setShowConfirmation(true);
+    }
+  }
 
-      let [input, setInput] = useState("")
-      function handleClick(value) {
+  const handleReset = () => {
+    setPickupLocation('');
+    setDropoffLocation('');
+    setDate('');
+    setTime('');
+    setAdditionalRequirements('');
+    setShowConfirmation(false);
+  }
 
-            setInput(input + value)
-
-      }
-      function calculate(value) {
-
-            let outputVal = eval(input)
-            setInput(outputVal)
-
-      }
-      function handleClear() {
-
-            setInput("")
-
-      }
-
-      return (
-            <div className="container">
-                  <h1>React Simple Calculator</h1>
-                  <div className="caculator">
-                        <input type="text" value={input} className="output" readOnly />
-                        <Keypad handleClick={handleClick} handleClear={handleClear} calculate={calculate}></Keypad>
-
-                  </div>
-
+  return (
+    <div className="App">
+      <div className='container'>
+        <h1 className='heading'>Taxi Inquiry Service</h1>
+        {showConfirmation ? (
+          <div>
+            <h2>Thank you for your inquiry!</h2>
+            <p>Pickup location: {pickupLocation}</p>
+            <p>Dropoff location: {dropoffLocation}</p>
+            <p>Date: {date}</p>
+            <p>Time: {time}</p>
+            <p>Additional requirements: {additionalRequirements}</p>
+            <button onClick={handleReset}>Submit another inquiry</button>
+          </div>
+      ) : (
+          <form onSubmit={handleSubmit}>
+            <div className='input'>
+            <label htmlFor="pickupLocation">Pickup location:</label>
+            <input type="text" id="pickupLocation" value={pickupLocation} onChange={(event) => setPickupLocation(event.target.value)} required />
             </div>
-      )
+            <br />
+            <div className='input'>
+            <label htmlFor="dropoffLocation">Dropoff location:</label>
+            <input type="text" id="dropoffLocation" value={dropoffLocation} onChange={(event) => setDropoffLocation(event.target.value)} required />
+            </div>
+            <br />
+            <div className='input'>
+            <label htmlFor="date">Date:</label>
+            <input type="date" id="date" value={date} onChange={(event) => setDate(event.target.value)} required />
+            </div>
+            <br />
+            <div className='input'>
+            <label htmlFor="time">Time:</label>
+            <input type="time" id="time" value={time} onChange={(event) => setTime(event.target.value)} required />
+            </div>
+            <br />
+            <div className='input'>
+            <label htmlFor="additionalRequirements">Additional requirements:</label>
+            <textarea id="additionalRequirements" value={additionalRequirements} onChange={(event) => setAdditionalRequirements(event.target.value)}></textarea>
+            </div>
+            <br />
+            <button className='button' type="submit">Submit</button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
